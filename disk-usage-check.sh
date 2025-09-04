39d093e9d7f1d10272e340f7a6e830094a2b8037
Carlos Eduardo <kadu.unisal1@gmail.com>
2025-09-04 17:40:33 -0300

    Update disk-usage-check.sh

--- disk-usage-check.sh
+++ disk-usage-check.sh
@@ -5,6 +5,7 @@
 #
 USE=`df -h | grep "/mnt/c" | awk '{print $5}' | tr -d '%'`
 
+
 if [[ $USE -gt 80 ]]; then
     #echo "Check you Disk space!!" | mailx -s "ACTION REQUIRED: Full disk alert" kadu.unisal1@gmail.com
     echo "Check your Disk Space!"
