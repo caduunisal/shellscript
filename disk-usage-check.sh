000593484496e70e8a00b6f461907ddf98c62299
Carlos Eduardo <kadu.unisal1@gmail.com>
2025-09-05 11:15:44 -0300

    Change alert message

--- disk-usage-check.sh
+++ disk-usage-check.sh
@@ -5,9 +5,9 @@
 #
 USE=`df -h | grep "/mnt/c" | awk '{print $5}' | tr -d '%'`
 
-if [[ $USE -gt 80 ]]; then
+if [[ $USE -gt 5 ]]; then
     #echo "Check you Disk space!!" | mailx -s "ACTION REQUIRED: Full disk alert" kadu.unisal1@gmail.com
-    echo "Check your Disk Space!"
+    echo "Check your Disk Space!" > /tmp/disk-usage.txt
 fi
 
 # Configuring Azure Devops Pipeline
