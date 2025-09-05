f93678392d3c63264559800b64004e5393e0cf24
Carlos Eduardo <kadu.unisal1@gmail.com>
2025-09-05 11:20:57 -0300

    Fix code to point to Azure Virtual Machine Disk Space parameters

--- disk-usage-check.sh
+++ disk-usage-check.sh
@@ -3,16 +3,14 @@
 #
 # Check Disk Usage and inform the user to take action
 #
-USE=`df -h | grep "/mnt/c" | awk '{print $5}' | tr -d '%'`
+#USE=`df -h | grep "/mnt/c" | awk '{print $5}' | tr -d '%'`
+USE=`df -h | grep "/dev/root" | awk '{print $5}' | tr -d '%'`
 
 if [[ $USE -gt 5 ]]; then
     #echo "Check you Disk space!!" | mailx -s "ACTION REQUIRED: Full disk alert" kadu.unisal1@gmail.com
     echo "Check your Disk Space!" > /tmp/disk-usage.txt
 fi
 
-# Configuring Azure Devops Pipeline
-# Create config.sh script
-# Remove remote files
 
 
 
